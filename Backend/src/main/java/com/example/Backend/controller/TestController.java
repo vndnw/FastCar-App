90f436acedf9457dbebe483d3360e13db93f837f
Vo Xuan Duong <xuanduong.261104@gmail.com>
2025-06-26 13:23:19 +0700

    fix complete payment

--- Backend/src/main/java/com/example/Backend/controller/TestController.java
+++ Backend/src/main/java/com/example/Backend/controller/TestController.java
@@ -3,9 +3,13 @@ package com.example.Backend.controller;
 
 import com.example.Backend.service.CloudinaryService;
 import com.example.Backend.service.EmailService;
+import com.example.Backend.service.VNPAYService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/test")
@@ -14,9 +18,11 @@ public class TestController {
     // In a real application, you would inject an email service and use it here
     private final EmailService emailService;
     private final CloudinaryService cloudinaryService;
-    public TestController(EmailService emailService, CloudinaryService cloudinaryService) {
+    public  final VNPAYService vnpayService;
+    public TestController(EmailService emailService, CloudinaryService cloudinaryService, VNPAYService vnpayService) {
         this.emailService = emailService;
         this.cloudinaryService = cloudinaryService;
+        this.vnpayService = vnpayService;
     }
     // Example endpoint to send a test email
      @GetMapping("/sendEmail")
@@ -29,4 +35,12 @@ public class TestController {
         String image = cloudinaryService.uploadImage(file);
         return ResponseEntity.ok(image);
      }
+
+     @GetMapping("/test-payment")
+        public ResponseEntity<?> testPayment(HttpServletRequest request) {
+            return ResponseEntity.ok(
+                vnpayService.generatePayUrl(request, BigDecimal.valueOf(100000), "123456789")
+            );
+        }
+
 }
