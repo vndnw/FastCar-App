f2c2adf12b5457867385a712ebebfb18eac32710
Duyi <2251120143@ut.edu.vn>
2025-06-30 22:52:09 +0700

    Remove dotenv loading logic from DotenvApplicationContextInitializer and update allowed CORS origins in application.properties

--- Backend/src/main/java/com/example/Backend/config/DotenvApplicationContextInitializer.java
+++ Backend/src/main/java/com/example/Backend/config/DotenvApplicationContextInitializer.java
@@ -10,11 +10,6 @@ public class DotenvApplicationContextInitializer implements ApplicationContextIn
 
     @Override
     public void initialize(ConfigurableApplicationContext context) {
-        Dotenv dotenv = Dotenv.load(); // Tự động đọc file .env
 
-        dotenv.entries().forEach(entry -> {
-            // Đặt mỗi key-value trong .env thành System property
-            System.setProperty(entry.getKey(), entry.getValue());
-        });
     }
 }
